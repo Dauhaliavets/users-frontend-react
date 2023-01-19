37bdb6dc18fbae4a43e14c432ed50892260df178
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2023-01-19 15:28:17 +0300

    feat: add base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import './App.css';
 
+import { Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/esm/Button';
+import { Outlet } from 'react-router';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header className="m-2">
+        <Container className="d-flex gap-2">
+          <Button variant="primary">SignIn</Button>
+          <Button variant="primary">SignUp</Button>
+        </Container>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>Created by Dzmitry Dauhaliavets</footer>
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,39 @@
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App';
+import HomePage from './components/pages/HomePage';
+import SignInForm from './components/pages/SignInForm';
+import SignUpForm from './components/pages/SignUpForm';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/signIn',
+        element: <SignInForm />,
+      },
+      {
+        path: '/signUp',
+        element: <SignUpForm />,
+      },
+      {
+        path: '/home',
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
   document.getElementById('root'),
 );
