e86453fe6720f1f03b7b16f372a6a6d173aa7df6
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2023-01-20 03:01:11 +0300

    fix: fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import './App.css';
 
 import { useState } from 'react';
-import { Outlet } from 'react-router';
+import { createRoutesFromElements, Outlet, Route } from 'react-router';
+import { createBrowserRouter } from 'react-router-dom';
 
 import Header from './components/header/Header';
+import HomePage from './components/pages/HomePage';
+import SignInForm from './components/pages/SignInForm';
+import SignUpForm from './components/pages/SignUpForm';
+import WelcomePage from './components/pages/WelcomePage';
 import { Context } from './context/context';
 import { IUser } from './models/UserModel';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<App />}>
+      <Route path="/" element={<WelcomePage />} />
+      <Route path="/signIn" element={<SignInForm />} />
+      <Route path="/signUp" element={<SignUpForm />} />
+      <Route path="/home" element={<HomePage />} />
+    </Route>,
+  ),
+);
 
 function App() {
   const [users, setUsers] = useState<IUser[]>([]);
@@ -30,4 +46,4 @@ function App() {
   );
 }
 
-export default App;
+export default router;
--- src/main.tsx
+++ src/main.tsx
@@ -3,33 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 
-import App from './App';
-import HomePage from './components/pages/HomePage';
-import SignInForm from './components/pages/SignInForm';
-import SignUpForm from './components/pages/SignUpForm';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/signIn',
-        element: <SignInForm />,
-      },
-      {
-        path: '/signUp',
-        element: <SignUpForm />,
-      },
-      {
-        path: '/home',
-        element: <HomePage />,
-      },
-    ],
-  },
-]);
+import router from './App';
 
 ReactDOM.render(
   <React.StrictMode>
